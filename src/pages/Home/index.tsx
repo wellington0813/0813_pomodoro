c9916c44ff72fcb5ac0d27b0b0193ed384271ffb
wellington0813 <2607wellington@gmail.com>
2024-05-24 08:15:53 -0300

    Feat: estilização do separador

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Play } from "phosphor-react";
-import { CountdownContainer, FormContainer, HomeContainer } from "./styles";
+import { CountdownContainer, FormContainer, HomeContainer, Separator } from "./styles";
 
 export function Home() {
     return (
@@ -20,7 +20,7 @@ export function Home() {
                 <CountdownContainer>
                     <span>0</span>
                     <span>0</span>
-                    <span>:</span>
+                    <Separator>:</Separator>
                     <span>0</span>
                     <span>0</span>
                 </CountdownContainer>
