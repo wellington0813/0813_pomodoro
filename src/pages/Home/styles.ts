98c13b80dd2db5f47d5cdfb39a2fc38798d4d6cf
wellington0813 <2607wellington@gmail.com>
2024-05-24 08:16:10 -0300

    Feat: commit maneiro

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -44,3 +44,15 @@ export const CountdownContainer = styled.main`
     }
 
 `
+
+export const Separator = styled.div`
+    padding: 2rem 0;
+    color: ${(props) => props.theme['green-500']};
+
+    width: 4rem;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+
+    
+`
