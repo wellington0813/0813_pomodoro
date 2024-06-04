fd74e93156089a8654773913adae0d151bdf29c8
wellington0813 <2607wellington@gmail.com>
2024-06-04 07:34:01 -0300

    Feat: Formulários - Validando formulários part 2

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,21 @@
 import { Play } from "phosphor-react";
 import { useForm } from 'react-hook-form'
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from 'zod'
+
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
 
 // controlled / uncontrolled
 
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, 'Informe a tarefa'),
+    minutesAmount: zod.number().min(5).max(60)
+})
+
 export function Home() {
-    const { register, handleSubmit, watch } = useForm()
+    const { register, handleSubmit, watch } = useForm({
+        resolver: zodResolver(newCycleFormValidationSchema),
+    })
 
     function handleCreateNewCycle(data: any) {
         console.log(data)
