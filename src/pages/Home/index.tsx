478d8d43d73d6567ae0b826b57be6286891a6d0e
wellington0813 <2607wellington@gmail.com>
2024-06-05 07:27:46 -0300

    Feat: Formulários - Resetando formulário

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,7 +18,7 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-    const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+    const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
         defaultValues: {
             task: '',
@@ -28,6 +28,7 @@ export function Home() {
 
     function handleCreateNewCycle(data: NewCycleFormData) {
         console.log(data)
+        reset()
     }
 
     const task = watch('task')
